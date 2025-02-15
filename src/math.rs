179db42fce9ca9aa2453ea3f7a949bdf3623bae6
Mattia Penati <mattia.penati@protonmail.com>
2025-02-15 04:04:10 +0100

    {2,3}-dimensional vector

--- src/math.rs
+++ src/math.rs
@@ -1,8 +1,10 @@
 //! A collection of tools for mathematical computation.
 
-pub use self::{angle::*, complex::*, traits::*};
+pub use self::{angle::*, complex::*, traits::*, vector2::*, vector3::*};
 
 mod angle;
 mod complex;
 mod macros;
 mod traits;
+mod vector2;
+mod vector3;
